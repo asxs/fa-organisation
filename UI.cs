48e0cffd3c753978ddaf5eb5dac7acd13b18b1b3
asxs <lherrmann@gmail.com>
2013-07-30 01:54:56 +0200

    Documentation
    Editing and documentation of the first elements

--- UI.cs
+++ UI.cs
@@ -42,57 +42,123 @@ namespace As
         }
     }
 
+    /// <summary>
+    /// Google GMail Property Package
+    /// </summary>
     public static class GMailPop3
     {
+        /// <summary>
+        /// Gets the POP3 Server from GMail
+        /// </summary>
         public const string Server = "pop.gmail.com";
+
+        /// <summary>
+        /// POP3 (SSL)
+        /// </summary>
         public const uint SslPort = 995;
+
+        /// <summary>
+        /// Gets or sets the client IP Address (for e.g. 192.168.xxx.xxx)
+        /// </summary>
         public static string Client { get; set; }
     }
 
+    /// <summary>
+    /// Gets the Token-Type for type of sending messages to a POP3 - Account
+    /// </summary>
     public enum PopTokenType : int
     {
+        /// <summary>
+        /// Authorization
+        /// </summary>
         Authorization = 0,
+
+        /// <summary>
+        /// Transaction
+        /// </summary>
         Transaction = 1,
+
+        /// <summary>
+        /// Default
+        /// </summary>
         None
     }
 
+    /// <summary>
+    /// POP3 Parameter with Tokens for Authentication or Transaction (for e.g. USER or PASS or LIST)
+    /// </summary>
     public interface IPopParameter
     {
+        /// <summary>
+        /// Gets or sets the POP3 Token
+        /// </summary>
         string Token { get; set; }
     }
 
-    public interface IPopAuthParameter : IPopParameter
+    /// <summary>
+    /// POP3 Authentication Parameter
+    /// </summary>
+    public interface IPopAuthParameter 
+        : IPopParameter
     {
+        /// <summary>
+        /// Gets the Token-Type for type of sending messages to a POP3 - Account
+        /// </summary>
         PopTokenType Type 
         { 
             get; 
             set; 
         }
 
+        /// <summary>
+        /// Gets all necessary authentication parameter
+        /// </summary>
+        /// <returns>Sequence with waiter</returns>
         IEnumerable<string> GetAuthParameter();
     }
 
-    public interface IPopTransactionParameter : IPopParameter
+    /// <summary>
+    /// POP3 Transaction Parameter
+    /// </summary>
+    public interface IPopTransactionParameter 
+        : IPopParameter
     {
+        /// <summary>
+        /// Gets the Token-Type for type of sending messages to a POP3 - Account
+        /// </summary>
         PopTokenType Type 
         { 
             get; 
             set; 
         }
 
+        /// <summary>
+        /// Gets all necessary transaction parameter
+        /// </summary>
+        /// <returns>Sequence with waiter</returns>
         IEnumerable<string> GetTransactionParameter();
     }
 
+    /// <summary>
+    /// Explicit use of Authentication or Transaction parameters for interfaces of IPopAuthParameter or IPopTransactionParameter
+    /// </summary>
     public sealed class PopParameter 
         : 
         IPopAuthParameter, 
         IPopTransactionParameter
     {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="token">Can be a custom token from the protocol type like USER or PASS or LIST</param>
         public PopParameter(string token)
         {
             Token = token;
         }
 
+        /// <summary>
+        /// Gets or sets the token for communication with the POP3 Server
+        /// </summary>
         public string Token 
         { 
             get; 
@@ -101,12 +167,19 @@ namespace As
 
         #region PopParameter (Authentication)
 
+        /// <summary>
+        /// Gets the Token-Type for type of sending messages to a POP3 - Account
+        /// </summary>
         PopTokenType IPopAuthParameter.Type
         {
             get;
             set;
         }
 
+        /// <summary>
+        /// Gets all necessary authentication parameter
+        /// </summary>
+        /// <returns>Sequence with waiter</returns>
         IEnumerable<string> IPopAuthParameter.GetAuthParameter()
         {
             foreach (var parameter in new string[] { Constants.UserAuthenticationString, Constants.PasswordAuthenticationString })
@@ -119,12 +192,19 @@ namespace As
 
         #region PopParameter (Transaction)
 
+        /// <summary>
+        /// Gets the Token-Type for type of sending messages to a POP3 - Account
+        /// </summary>
         PopTokenType IPopTransactionParameter.Type
         {
             get;
             set;
         }
 
+        /// <summary>
+        /// Gets all necessary transaction parameter
+        /// </summary>
+        /// <returns>Sequence with waiter</returns>
         IEnumerable<string> IPopTransactionParameter.GetAuthParameter()
         {
             foreach (var parameter in new string[] { Constants.ListTokenString })
@@ -136,9 +216,15 @@ namespace As
         #endregion
     }
 
+    /// <summary>
+    /// Little GMail Client that receives only a List of E-Mails
+    /// </summary>
     public class GMailClient 
         : IDisposable
     {
+        /// <summary>
+        /// Constructor
+        /// </summary>
         public GMailClient()
         {
 
@@ -146,12 +232,18 @@ namespace As
 
         #region GMailClient (IDisposable)
 
+        /// <summary>
+        /// Disposing all save or unsaved components
+        /// </summary>
         public void Dispose()
         {
             Dispose(true);
             GC.SuppressFinalize(this);
         }
 
+        /// <summary>
+        /// Disposing all save or unsaved components
+        /// </summary>
         protected void Dispose(bool disposing)
         {
             if (disposing)
@@ -181,12 +273,18 @@ namespace As
 
         #region Pop3Lite (IDisposable)
 
+        /// <summary>
+        /// Disposing all save or unsaved components
+        /// </summary>
         public void Dispose()
         {
             GC.SuppressFinalize(this);
             Dispose(true);
         }
 
+        /// <summary>
+        /// Disposing all save or unsaved components
+        /// </summary>
         protected void Dispose(bool disposing)
         {
             if (disposing)
@@ -198,6 +296,9 @@ namespace As
 
         #endregion
 
+        /// <summary>
+        /// Open a connection with given information about User and Password (with Authentication)
+        /// </summary>
         public void Open()
         {
             try
@@ -210,6 +311,9 @@ namespace As
             }
         }
 
+        /// <summary>
+        /// Open a connection with Authentication
+        /// </summary>
         public void OpenWithAuthentication()
         {
             Open();
@@ -223,6 +327,9 @@ namespace As
             SendAuthentication(authentication);
         }
 
+        /// <summary>
+        /// Waits for a receive event on POP-Client
+        /// </summary>
         private void WaitOnToken()
         {
             receiverThread = 
@@ -241,6 +348,9 @@ namespace As
             waitToken = -1;
         }
 
+        /// <summary>
+        /// Looks in the UDP-Client (POP-Client) if some data is available
+        /// </summary>
         private void WaitOnClientTokenInternal()
         {
             while (true)
@@ -253,6 +363,9 @@ namespace As
             }
         }
 
+        /// <summary>
+        /// Closes the POP-Client
+        /// </summary>
         public void Close()
         {
             if (client == null)
@@ -265,6 +378,10 @@ namespace As
             catch { }
         }
 
+        /// <summary>
+        /// Sending information about a transaction (for e.g. LIST)
+        /// </summary>
+        /// <param name="parameter">Gets all transaction parameters</param>
         public void SendTransaction(IPopTransactionParameter parameter)
         {
             foreach (var command in parameter.GetTransactionParameter())
@@ -274,6 +391,10 @@ namespace As
                     );
         }
 
+        /// <summary>
+        /// Sending information about authentications with USER and PASS
+        /// </summary>
+        /// <param name="parameter">Gets all authentication parameters USER and PASS</param>
         public void SendAuthentication(IPopAuthParameter parameter)
         {
             foreach (var command in parameter.GetAuthParameter())
@@ -287,6 +408,10 @@ namespace As
             }
         }
 
+        /// <summary>
+        /// Sending some custom data with the UDP-Client (POP-Client)
+        /// </summary>
+        /// <param name="parameter">Gets only one Token to send</param>
         internal void SendToServerInternal(IPopParameter parameter)
         {
             var byteCount 
@@ -305,6 +430,9 @@ namespace As
             client.EndSend(asyncResult);
         }
 
+        /// <summary>
+        /// Opens a UDP-Client with given local IP-Address
+        /// </summary>
         internal void OpenClientInternal()
         {
             try
@@ -320,6 +448,12 @@ namespace As
             }
         }
 
+        /// <summary>
+        /// Converts a Unicode-String in a byte-sequence and returns a out-value with the byte-count
+        /// </summary>
+        /// <param name="item">Unicode-String that should convert</param>
+        /// <param name="byteCount">Unicode-String that returns with the conversion</param>
+        /// <returns>The given byte-sequence from "item"</returns>
         private byte[] ConvertToByteSequence(string item, out int byteCount)
         {
             byteCount = -1;
@@ -330,10 +464,24 @@ namespace As
         }
     }
 
+    /// <summary>
+    /// Constants for the Process
+    /// </summary>
     public static class Constants
     {
+        /// <summary>
+        /// USER
+        /// </summary>
         public const string UserAuthenticationString = "USER";
+
+        /// <summary>
+        /// PASS
+        /// </summary>
         public const string PasswordAuthenticationString = "PASS";
+
+        /// <summary>
+        /// LIST
+        /// </summary>
         public const string ListTokenString = "LIST";
     }
 
