45a3e9173797fe50446a38d7b2ae10941a3e33c8
asxs <lherrmann@gmail.com>
2013-07-30 01:19:55 +0200

    First
    First commit for initialization. All of these files are in a editing
    mode

---
+++ UI.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace As
+{
+    partial class UI
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UI));
+            this.btnPopTest = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPopTest
+            // 
+            this.btnPopTest.Location = new System.Drawing.Point(752, 12);
+            this.btnPopTest.Name = "btnPopTest";
+            this.btnPopTest.Size = new System.Drawing.Size(107, 25);
+            this.btnPopTest.TabIndex = 0;
+            this.btnPopTest.Text = "&POP3 - Test";
+            this.btnPopTest.UseVisualStyleBackColor = true;
+            this.btnPopTest.Click += new System.EventHandler(this.btnPopTest_Click);
+            // 
+            // UI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(871, 487);
+            this.Controls.Add(this.btnPopTest);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "UI";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Organisation von Bewerbungen - Lars Ulrich Herrmann (c) 2013";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPopTest;
+    }
+}
+
---
+++ UI.cs
@@ -0,0 +1,344 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Net;
+using System.Net.Sockets;
+using System.Net.Security;
+using System.Net.Mail;
+
+using System.Threading;
+
+namespace As
+{
+    public partial class UI : Form
+    {
+        public UI()
+        {
+            InitializeComponent();
+        }
+
+        //Portnumber for POP3 SSL is 995
+        //Can connect with UDP, because it is not security question and here we need only the packages, without checking them in total way
+        //Local ip address 192.168.83.100
+        //SendAuthorization
+        //SendTransaction
+
+        private void btnPopTest_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+
+    public static class GMailPop3
+    {
+        public const string Server = "pop.gmail.com";
+        public const uint SslPort = 995;
+        public static string Client { get; set; }
+    }
+
+    public enum PopTokenType : int
+    {
+        Authorization = 0,
+        Transaction = 1,
+        None
+    }
+
+    public interface IPopParameter
+    {
+        string Token { get; set; }
+    }
+
+    public interface IPopAuthParameter : IPopParameter
+    {
+        PopTokenType Type 
+        { 
+            get; 
+            set; 
+        }
+
+        IEnumerable<string> GetAuthParameter();
+    }
+
+    public interface IPopTransactionParameter : IPopParameter
+    {
+        PopTokenType Type 
+        { 
+            get; 
+            set; 
+        }
+
+        IEnumerable<string> GetTransactionParameter();
+    }
+
+    public sealed class PopParameter 
+        : 
+        IPopAuthParameter, 
+        IPopTransactionParameter
+    {
+        public PopParameter(string token)
+        {
+            Token = token;
+        }
+
+        public string Token 
+        { 
+            get; 
+            set; 
+        }
+
+        #region PopParameter (Authentication)
+
+        PopTokenType IPopAuthParameter.Type
+        {
+            get;
+            set;
+        }
+
+        IEnumerable<string> IPopAuthParameter.GetAuthParameter()
+        {
+            foreach (var parameter in new string[] { Constants.UserAuthenticationString, Constants.PasswordAuthenticationString })
+                yield return parameter;
+
+            yield break;
+        }
+
+        #endregion
+
+        #region PopParameter (Transaction)
+
+        PopTokenType IPopTransactionParameter.Type
+        {
+            get;
+            set;
+        }
+
+        IEnumerable<string> IPopTransactionParameter.GetAuthParameter()
+        {
+            foreach (var parameter in new string[] { Constants.ListTokenString })
+                yield return parameter;
+
+            yield break;
+        }
+
+        #endregion
+    }
+
+    public class GMailClient 
+        : IDisposable
+    {
+        public GMailClient()
+        {
+
+        }
+
+        #region GMailClient (IDisposable)
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+
+            }
+        }
+
+        #endregion
+    }
+
+    public class Pop3Lite 
+        : IDisposable
+    {
+        private UdpClient 
+            client = null;
+
+        private IPEndPoint address = null;
+        private Thread receiverThread = null;
+
+        public Pop3Lite()
+        {
+            
+        }
+
+        internal static int waitToken = -1;
+
+        #region Pop3Lite (IDisposable)
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+            Dispose(true);
+        }
+
+        protected void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                client = null;
+                address = null;
+            }
+        }
+
+        #endregion
+
+        public void Open()
+        {
+            try
+            {
+                OpenClientInternal();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public void OpenWithAuthentication()
+        {
+            Open();
+
+            IPopAuthParameter authentication =
+                              new PopParameter(Constants.UserAuthenticationString);
+
+            authentication.
+                Type = PopTokenType.Authorization;
+            
+            SendAuthentication(authentication);
+        }
+
+        private void WaitOnToken()
+        {
+            receiverThread = 
+                new Thread(new ThreadStart(WaitOnClientTokenInternal));
+            receiverThread.Start();
+
+            while (true)
+            {
+                if (waitToken != -1)
+                    break;
+
+                Application.DoEvents();
+                Thread.Sleep(new TimeSpan(500));
+            }
+
+            waitToken = -1;
+        }
+
+        private void WaitOnClientTokenInternal()
+        {
+            while (true)
+            {
+                if (client.Available > 0)
+                {
+                    waitToken = 1;
+                    return;
+                }
+            }
+        }
+
+        public void Close()
+        {
+            if (client == null)
+                return;
+
+            try
+            {
+                client.Close();
+            }
+            catch { }
+        }
+
+        public void SendTransaction(IPopTransactionParameter parameter)
+        {
+            foreach (var command in parameter.GetTransactionParameter())
+                SendToServerInternal
+                    (
+                        new PopParameter(command)
+                    );
+        }
+
+        public void SendAuthentication(IPopAuthParameter parameter)
+        {
+            foreach (var command in parameter.GetAuthParameter())
+            {
+                SendToServerInternal
+                    (
+                        new PopParameter(command)
+                    );
+
+                WaitOnToken();
+            }
+        }
+
+        internal void SendToServerInternal(IPopParameter parameter)
+        {
+            var byteCount 
+                = -1;
+            var bytes = ConvertToByteSequence(parameter.Token, out byteCount);
+
+            var asyncResult = 
+                client.BeginSend(bytes, byteCount, null, null);
+            
+            while (!asyncResult.IsCompleted)
+            {
+                Application.DoEvents();
+                Thread.Sleep(new TimeSpan(500));
+            }
+
+            client.EndSend(asyncResult);
+        }
+
+        internal void OpenClientInternal()
+        {
+            try
+            {
+                client = new UdpClient
+                ((
+                       address = new IPEndPoint(IPAddress.Parse(GMailPop3.Client), 0)
+                ));
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private byte[] ConvertToByteSequence(string item, out int byteCount)
+        {
+            byteCount = -1;
+            var bytes = Encoding.Default.GetBytes(item);
+            byteCount = bytes.Length;
+
+            return bytes;
+        }
+    }
+
+    public static class Constants
+    {
+        public const string UserAuthenticationString = "USER";
+        public const string PasswordAuthenticationString = "PASS";
+        public const string ListTokenString = "LIST";
+    }
+
+    public static class Extensions
+    {
+
+    }
+}
