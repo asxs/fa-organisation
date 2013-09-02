c911aa1555c72c1511e93a92f36eb847e864615a
asxs <lherrmann@gmail.com>
2013-09-02 19:59:12 +0200

    Leave Copyright Comment Part XN

--- Sense Organisation Assist/Data/Transactions/Microsoft/SqlServerTransactionSystem.cs
+++ Sense Organisation Assist/Data/Transactions/Microsoft/SqlServerTransactionSystem.cs
@@ -1,7 +1,20 @@
-﻿using System;
+﻿/* 
+ * Builded by Lars Ulrich Herrmann (c) 2013 with f.fN. Sense Applications in year August 2013
+ * The code can be used in other Applications if it makes sense
+ * If it makes sense the code can be used in this Application
+ * I hold the rights on all lines of code and if it makes sense you can contact me over the publish site
+ * Feel free to leave a comment
+ * 
+ * Good Bye
+ * 
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data;
+using System.Data.Common;
 
 namespace IxSApp
 {
